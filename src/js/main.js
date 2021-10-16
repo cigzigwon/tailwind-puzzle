09ffb359f383cc5856be5433b1c818bc8c6de702
Chris Sobeck <csobeck@pyroclasti.cloud>
2021-10-16 15:53:55 -0400

    Minesweeper

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,33 @@
-import React, { useEffect, useRef } from "react"
+import React, { useEffect, useState } from "react"
 import ReactDOM from "react-dom"
 import interact from "interactjs"
 
 import '../sass/styles.sass'
 
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * max)
+}
+
+var x_set = [0, 1, 2, 3, 4, 5]
+var y_set = Array.from(x_set)
+
+const max_bombs = 8
+const grid = Array.from(x_set).fill(Array.from(y_set).fill(0))
+const mines = new Array(max_bombs).fill().reduce((prev, curr) => {
+	const x_key = getRandomInt(x_set.length)
+	const y_key = getRandomInt(y_set.length)
+	const x = x_set[x_key]
+	const y = y_set[y_key]
+
+	if (prev.find(([a, b]) => a == x && b == y)) {
+		return prev
+	}
+
+	return prev.concat([[x, y]])
+}, [])
+
 const App = () => {
+	const [blasts, set_blasts] = useState(0)
 	const dragMoveListener = (event) => {
 	  var target = event.target
 	  // keep the dragged position in the data-x/data-y attributes
@@ -46,16 +69,28 @@ const App = () => {
 		    // feedback the possibility of a drop
 		    dropzoneElement.classList.add('drop-target')
 		    draggableElement.classList.add('can-drop')
-		    draggableElement.textContent = 'Dragged in'
+		    // draggableElement.textContent = ' '
 		  },
 		  ondragleave: function (event) {
 		    // remove the drop feedback style
 		    event.target.classList.remove('drop-target')
 		    event.relatedTarget.classList.remove('can-drop')
-		    event.relatedTarget.textContent = 'Dragged out'
+		    // event.target.classList.remove('dropped')
+		    // event.relatedTarget.textContent = ' '
 		  },
 		  ondrop: function (event) {
-		    event.relatedTarget.textContent = 'Dropped'
+		  	var dropzoneElement = event.target
+		    // dropzoneElement.textContent = ' '
+		    dropzoneElement.classList.add('dropped')
+		    dropzoneElement.classList.remove('bg-blue-400')
+
+		    if (dropzoneElement.classList.contains('xxx')) {
+		    	dropzoneElement.classList.add('bg-red-400')
+		    	dropzoneElement.classList.remove('xxx')
+		    	set_blasts(prev => ++prev)
+		    } else {
+		    	dropzoneElement.classList.add('bg-gray-300')
+		    }
 		  },
 		  ondropdeactivate: function (event) {
 		    // remove active dropzone feedback
@@ -79,15 +114,32 @@ const App = () => {
 		  })
 	}, [])
 
+	useEffect(() => {
+		if (blasts >= mines.length) {
+			setTimeout(() => {
+				alert("YOU LOSE")
+				location.reload()
+			}, 400)
+		}
+	}, [blasts])
+
 	return (
 		<>
-			<div id="no-drop" class="drag-drop"> #no-drop </div>
+			<div id="yes-drop" className="drag-drop h-6 w-6 bg-yellow-800"></div>
+			<div className="container mt-24 mx-auto px-6 md:w-1/3">
+				<h1 className="mb-6 text-3xl">DnD Mine Sweeper</h1>
+				{grid.map((y, y_key) => (
+					<div key={y_key} className="flex flex-row">
+					  {y.map((x, x_key) => {
+					  	const found = mines.find(([x, y]) => (x == x_key && y == y_key))
+					  	const classes = "dropzone inline-flex flex-grow items-center justify-center p-4 m-1 bg-blue-400" + (found ? " xxx" : "")
 
-			<div id="yes-drop" class="drag-drop"> #yes-drop </div>
-
-			<div id="outer-dropzone" class="dropzone">
-			  #outer-dropzone
-			  <div id="inner-dropzone" class="dropzone">#inner-dropzone</div>
+					  	return (
+						  	<div key={x_key} className={classes}><span>&nbsp;</span></div>
+						  )
+					  })}
+					</div>
+				))}
 			</div>
 		</>
 	)
